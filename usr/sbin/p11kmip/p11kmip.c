9fdafb0554ff73d826e5960ed7a1512223a6b520
john-craig <john.craig@ibm.com>
2024-01-02 13:19:44 -0500

    Call KMIP key format functions with correct argument

--- usr/sbin/p11kmip/p11kmip.c
+++ usr/sbin/p11kmip/p11kmip.c
@@ -4414,7 +4414,7 @@ static CK_RV p11kmip_retrieve_remote_public_key(const struct p11kmip_keytype
 
     switch (kmip_wrap_key_format) {
     case KMIP_KEY_FORMAT_TYPE_PKCS_1:
-        rc = kmip_get_pkcs1_public_key(kval, algo, pub_key);
+        rc = kmip_get_pkcs1_public_key(key, algo, pub_key);
         if (rc != CKR_OK) {
             warnx("Failed to get RSA public key parts");
             rc = -EIO;
@@ -4422,7 +4422,7 @@ static CK_RV p11kmip_retrieve_remote_public_key(const struct p11kmip_keytype
         }
         break;
     case KMIP_KEY_FORMAT_TYPE_PKCS_8:
-        rc = kmip_get_pkcs8_public_key(kval, pub_key);
+        rc = kmip_get_pkcs8_public_key(key, pub_key);
         if (rc != CKR_OK) {
             warnx("Failed to get RSA public key parts");
             rc = -EIO;
@@ -4430,7 +4430,7 @@ static CK_RV p11kmip_retrieve_remote_public_key(const struct p11kmip_keytype
         }
         break;
     case KMIP_KEY_FORMAT_TYPE_TRANSPARENT_RSA_PUBLIC_KEY:
-        rc = kmip_get_transparent_rsa_public_key(kval, &modulus, &pub_exp);
+        rc = kmip_get_transparent_rsa_public_key(key, &modulus, &pub_exp);
         if (rc != CKR_OK) {
             warnx("Failed to get RSA public key parts");
             rc = -EIO;
