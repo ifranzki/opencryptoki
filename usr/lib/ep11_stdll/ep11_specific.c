6d97d31da2553112f0584bed9e804d5d3151c41a
Ingo Franzki <ifranzki@linux.ibm.com>
2024-01-12 09:36:27 +0100

    Constant time fixes for C_Decrypt return code handling
    Return code handling of C_Decrypt, C_DecryptUpdate, and C_DecryptFinal must
    be performed in a constant time manner for RSA mechanisms. Otherwise it
    may cause a timing side channel that may be used to perform a Bleichenbacher
    style attack.
    
    Handling of error situations with CKR_BUFFER_TOO_SMALL or size-query calls,
    where the output buffer is NULL and the required size of the output buffer
    is to be returned, do not need to be performed in constant time, since
    these cases are shortcut anyway, and the result is only dependent on the
    modulus size of the RSA key (which is public information anyway).
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/ep11_stdll/ep11_specific.c
+++ usr/lib/ep11_stdll/ep11_specific.c
@@ -8034,10 +8034,12 @@ CK_RV ep11tok_decrypt_final(STDLL_TokData_t * tokdata, SESSION * session,
     rc = constant_time_select(constant_time_eq(rc, CKR_OK),
                               ep11_error_to_pkcs11_error(rc, session),
                               rc);
-    if (rc != CKR_OK) {
-        TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
-    } else {
-        TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+    if (!is_rsa_mechanism(ctx->mech.mechanism)) {
+        if (rc != CKR_OK) {
+            TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
+        } else {
+            TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+        }
     }
 
 done:
@@ -8083,10 +8085,12 @@ CK_RV ep11tok_decrypt(STDLL_TokData_t * tokdata, SESSION * session,
     rc = constant_time_select(constant_time_eq(rc, CKR_OK),
                               ep11_error_to_pkcs11_error(rc, session),
                               rc);
-    if (rc != CKR_OK) {
-        TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
-    } else {
-        TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+    if (!is_rsa_mechanism(ctx->mech.mechanism)) {
+        if (rc != CKR_OK) {
+            TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
+        } else {
+            TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+        }
     }
 
 done:
@@ -8138,10 +8142,12 @@ CK_RV ep11tok_decrypt_update(STDLL_TokData_t * tokdata, SESSION * session,
     rc = constant_time_select(constant_time_eq(rc, CKR_OK),
                               ep11_error_to_pkcs11_error(rc, session),
                               rc);
-    if (rc != CKR_OK) {
-        TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
-    } else {
-        TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+    if (!is_rsa_mechanism(ctx->mech.mechanism)) {
+        if (rc != CKR_OK) {
+            TRACE_ERROR("%s rc=0x%lx\n", __func__, rc);
+        } else {
+            TRACE_INFO("%s rc=0x%lx\n", __func__, rc);
+        }
     }
 
 done:
