d49f01df3ca37ad46e99fb127f41a2303b4794e0
Ingo Franzki <ifranzki@linux.ibm.com>
2026-02-18 11:06:49 +0100

    CCA: Add support for keygen and import of PKCS#11 v3.2 ML-KEM keys
    CCA v8.5 or later is required to support ML-KEM.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

--- usr/lib/cca_stdll/tok_struct.h
+++ usr/lib/cca_stdll/tok_struct.h
@@ -149,7 +149,7 @@ token_spec_t token_specific = {
     &token_specific_ml_dsa_generate_keypair,
     &token_specific_ml_dsa_sign,
     &token_specific_ml_dsa_verify,
-    NULL,                       // ml_kem_generate_keypair
+    &token_specific_ml_kem_generate_keypair,
     NULL,                       // ml_kem_encapsulate_key
     NULL,                       // ml_kem_decapsulate_key
     &token_specific_get_mechanism_list,
